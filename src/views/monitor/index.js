79860125c6b6f6db66a855ae0db05bea70947c08
Juan David Polo <juandavidpolomendez@gmail.com>
2025-01-06 19:37:33 -0500

    feat: minor fix

--- src/views/monitor/index.js
+++ src/views/monitor/index.js
@@ -4,7 +4,6 @@ import { Row } from "reactstrap";
 import { useLocation } from 'react-router-dom';
 
 import Sidebar from "src/components/layout/sidebar";
-import CustomModal from "src/components/customModal";
 import ViewHeader from "src/components/viewHeader";
 import { DataCard, ListCard } from "src/components/cards";
 import { warning, danger } from "src/utils/getDefinedStyles";
