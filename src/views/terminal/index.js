70703a76b047deaa278218e810e5fb90343af5be
Juan David Polo <juandavidpolomendez@gmail.com>
2025-01-08 13:55:53 -0500

    fix: basic terminal

--- src/views/terminal/index.js
+++ src/views/terminal/index.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Terminal } from 'xterm';
+import { WebLinksAddon } from 'xterm-addon-web-links';
 
 import Sidebar from "src/components/layout/sidebar";
 
-const Terminal = () => {
-  return(
+const Terminals = () => {
+  const terminalRef = useRef(null);
+
+  useEffect(() => {
+    const terminal = new Terminal();
+    terminal.loadAddon(new WebLinksAddon());
+
+    if (terminalRef.current) {
+      terminal.open(terminalRef.current);
+      terminal.writeln('Hello world');
+    }
+
+    return () => {
+      terminal.dispose();
+    };
+  }, []);
+
+  return (
     <>
-      <Sidebar/>
+      <Sidebar />
       <div className="main--section configure--view">
-        Terminal View
+        Terminal
+        <div ref={terminalRef} 
+        style={{ height: "300px", width: "300px", background: "#000" }}/>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Terminal
+export default Terminals;
